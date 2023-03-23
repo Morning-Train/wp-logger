fb53129b64f7b4065d5f930cf3423c315aae0240
Mathias <mathias.barentsen@gmail.com>
2023-03-23 23:24:17 +0100

    ✨ Add logic to handle FileLogger

--- src/Loggers/FileLogger.php
+++ src/Loggers/FileLogger.php
@@ -4,10 +4,39 @@ namespace Morningtrain\WP\Logger\Loggers;
 
 use Morningtrain\WP\Logger\Abstracts\AbstractLeveledLogger;
 
-class FileLogger extends AbstractLeveledLogger {
+class FileLogger extends AbstractLeveledLogger
+{
+    private string $path;
+
+    public function __construct(
+        string $path
+    )
+    {
+        $this->path = rtrim($path, '/\\') . '/';
+        $this->path .= date('Y') . '/';
+        $this->path .= date('m');
+    }
 
     public function log($level, \Stringable|string $message, array $context = []): void
     {
+        if (! file_exists($this->path)) {
+            mkdir($this->path, 0777, true);
+        }
 
+        $logContent = '[DateTime]: ';
+        $logContent .= date('Y-m-d H:i:s P T');
+        $logContent .= PHP_EOL;
+
+        $logContent .= '[Message]: ';
+        $logContent .= $message;
+        $logContent .= PHP_EOL;
+
+        $logContent .= '[Context]: ';
+        $logContent .= print_r($context, true);
+        $logContent .= PHP_EOL;
+
+        $filename = $this->path . '/' . date('d') . '.txt';
+
+        file_put_contents($filename, $logContent, FILE_APPEND);
     }
 }
