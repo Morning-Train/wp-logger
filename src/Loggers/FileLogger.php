fc0d5a6d629fcc19805a3bfecfb9139cb670964e
Martin Schadegg Brønniche <ms@morningtrain.dk>
2023-06-08 13:33:50 +0200

    :recycle: Changed format of logs in file logger

--- src/Loggers/FileLogger.php
+++ src/Loggers/FileLogger.php
@@ -18,17 +18,19 @@ class FileLogger extends AbstractLeveledLogger
             mkdir($parentPath, 0660, true);
         }
 
-        $logContent = '[DateTime]: ';
-        $logContent .= date('Y-m-d H:i:s P T');
+        $date = current_datetime();
+
+        $logContent = "{$date->format('Y-m-d H:i:s P T')} - {$level}: ";
         $logContent .= PHP_EOL;
 
-        $logContent .= '[Message]: ';
-        $logContent .= $message;
+        $logContent .= "    [Message]: {$message}";
         $logContent .= PHP_EOL;
 
-        $logContent .= '[Context]: ';
-        $logContent .= print_r($context, true);
-        $logContent .= PHP_EOL;
+        if(!empty($context)) {
+            $logContent .= '    [Context]: ';
+            $logContent .= str_replace("\n", "\n    ", print_r($context, true));
+            $logContent .= PHP_EOL;
+        }
 
         file_put_contents($this->filename, $logContent, FILE_APPEND);
     }
